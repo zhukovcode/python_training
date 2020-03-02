5e29b1be62ffc5a952538d2ad335ffa43b419361
Zhukov Kirill <zhukov.k.a@effective-group.ru>
2020-03-02 18:19:08 +0300

    установка виртуального окружения в проект

--- test_lifehacker.py
+++ test_lifehacker.py
@@ -9,26 +9,41 @@ import unittest, time, re
 
 class LifehackerTest(unittest.TestCase):
     def setUp(self):
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
-
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(30)
+        self.base_url = "https://www.google.com/"
+        self.verificationErrors = []
+        self.accept_next_alert = True
+    
     def test_lifehacker(self):
-        wd = self.wd
-        wd.get("https://lifehacker.ru/")
-        wd.find_element_by_link_text(u"💳Промокоды").click()
+        driver = self.driver
+        driver.get("https://lifehacker.ru/")
+        driver.find_element_by_link_text(u"💳Промокоды").click()
     
     def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
+        try: self.driver.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
         return True
     
     def is_alert_present(self):
-        try: self.wd.switch_to_alert()
+        try: self.driver.switch_to_alert()
         except NoAlertPresentException as e: return False
         return True
     
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally: self.accept_next_alert = True
+    
     def tearDown(self):
-        self.wd.quit()
+        self.driver.quit()
+        self.assertEqual([], self.verificationErrors)
 
 if __name__ == "__main__":
     unittest.main()
